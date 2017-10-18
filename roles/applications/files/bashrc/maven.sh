5617ed94f66a31af4d1d0d8bcba1568d1df47925
Guilherme Victal <guilhermevictal em gmail ponto com>
2017-10-18 11:05:45 -0200

    Use ansible_env instead of lookups

--- roles/applications/files/bashrc/maven.sh
+++ roles/applications/files/bashrc/maven.sh
@@ -1,5 +1,5 @@
 export M2_HOME="/home/guilherme/applications/apache-maven-3.3.9"
-export MAVEN_OPTS="-Xms256M -Xmx1024M  -XX:MaxPermSize=256M" #-Dfile.encoding=UTF-8 -Dclient.encoding.override=UTF-8"
+export MAVEN_OPTS="-Xms256M -Xmx1368M  -XX:MaxPermSize=256M" #-Dfile.encoding=UTF-8 -Dclient.encoding.override=UTF-8"
 export PATH=$M2_HOME/bin:$PATH
 
 
