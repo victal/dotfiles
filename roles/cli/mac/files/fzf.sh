90d713ea008a94e4472d4f202633e4df5df12805
Guilherme Victal <guilherme.costa@nexadigital.com.br>
2020-10-22 09:00:22 -0300

    Use ag with fzf

--- roles/cli/mac/files/fzf.sh
+++ roles/cli/mac/files/fzf.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# TODO: Use fd, ag or another command that accepts an ignore list instead of find
 _fzf_compgen_path() {
   echo "$1"
   command find -L "$1" \
@@ -12,6 +11,22 @@ _fzf_compgen_dir() {
   command find -L "$1" \
     -name .git -prune -o -name .hg -prune -o -name .svn -prune -o -name node_modules -prune -o -type d \
     -a -not -path "$1" -print 2> /dev/null | sed 's@^\./@@'
+    }
+
+#determines search program for fzf
+if type ag &> /dev/null; then
+  export FZF_DEFAULT_COMMAND='ag -g ""'
+  _fzf_compgen_path() {
+    echo "$1"
+    command ag --ignore .git --hidden -g "$1" 2> /dev/null | sed 's@^\./@@'
   }
+
+  _fzf_compgen_dir() {
+    command ag --ignore .git --hidden -g "$1" 2> /dev/null | sed 's@^\./@@'
+  }
+#prefer rg over ag
+elif type rg &> /dev/null; then
+  export FZF_DEFAULT_COMMAND='rg --files --hidden'
+fi
 
 [ -f ~/.fzf.bash ] && source ~/.fzf.bash
